0dd88a1dee4ea6498d037323b68fe25d68daf6c3
Mesgun <Mesgun@MESGUN->
2026-05-10 02:36:54 +0200

    Add project files.

---
+++ BookQuoteApi/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using BookQuoteApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookQuoteApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> books = new List<Book>();
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            books.Add(book);
+
+            return Ok(book);
+        }
+    }
+}
