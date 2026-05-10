82513a960a580b5ab10125312b9604def82af69b
Mesgun <Mesgun@MESGUN->
2026-05-10 18:12:40 +0200

    Add quote model and basic quote endpoints

---
+++ BookQuoteApi/Controllers/QuoteController.cs
@@ -0,0 +1,33 @@
+﻿using BookQuoteApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookQuoteApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuoteController : ControllerBase
+    {
+        private static List<Quote> quotes = new List<Quote>();
+
+        [HttpGet]
+        public IActionResult GetAllQuotes()
+        {
+            return Ok(quotes);
+        }
+
+        [HttpPost]
+        public IActionResult AddQuote(Quote quote)
+        {
+            if (string.IsNullOrWhiteSpace(quote.Text))
+            {
+                return BadRequest("Quote text is required.");
+            }
+
+            quote.Id = quotes.Count + 1;
+
+            quotes.Add(quote);
+
+            return Ok(quote);
+        }
+    }
+}
