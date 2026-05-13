343b474600a120d98a89cf4f655a664fb0b7157e
Mesgun <Mesgun@MESGUN->
2026-05-13 03:14:06 +0200

    add jwt

--- book-quote-client/src/app/services/book.service.ts
+++ book-quote-client/src/app/services/book.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from '../models/book';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,20 @@ import { Book } from '../models/book';
 export class BookService {
   private apiUrl = 'http://localhost:5138/api/Book';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = this.authService.getToken();
+    return token
+      ? new HttpHeaders({ Authorization: `Bearer ${token}` })
+      : new HttpHeaders();
+  }
 
   getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>(this.apiUrl);
+    const headers = this.getAuthHeaders();
+    return this.http.get<Book[]>(this.apiUrl, { headers });
   }
 }
